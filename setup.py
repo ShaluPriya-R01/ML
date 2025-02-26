763f455523cf52b2307b65ab62dad7481249798d
Shalu Priya <shalupriya.r2021@kgkite.ac.in>
2025-02-26 11:19:54 +0530

    commit

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_requirements(file_path:str)-> List[str]:
 
 setup(
 name="mlproject",
-version="0.0.1",
+version="0.0.1", 
 author="Me",
 author_email="shalupriyaravishankar@gmail.com",
 packages=find_packages(),
